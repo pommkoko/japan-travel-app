6cf0e516b1df9ee49f19117be0d44e05ad848227
pommkoko <156825948+pommkoko@users.noreply.github.com>
2026-08-08 16:31:01 +0700

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// วาง URL และ Anon Key ที่ก๊อปปี้มาจาก Supabase ใน Step 1.4
-const supabaseUrl = 'https://eyguvgvncnqauizldkiv.supabase.co'
-const supabaseAnonKey = 'วeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5Z3V2Z3ZuY25xYXVpemxka2l2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3OTAxMzYsImV4cCI6MjEwMTM2NjEzNn0.xkf1D6PXjMupFLOWepK7wOkYgvjSCPvQlNTHiWGdBDs'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
